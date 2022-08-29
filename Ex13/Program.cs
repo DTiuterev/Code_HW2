c97a9fe13a4ff8b3593f7f147813e246a8645cc6
Dmitrii <dtiuterev@rambler.ru>
2022-08-29 11:14:26 +0300

    added Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа выводит третью цифру вводимого числа или сообщает, что ее нет
+Console.WriteLine("Введите число, а я напишу его третью цифру: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2)
+Console.WriteLine("Третья цифра этого числа " +NumberText[2]);
+else
+Console.WriteLine("У введенного числа нет третьей цифры");
