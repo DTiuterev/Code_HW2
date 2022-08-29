754b8a133f35a64cb36b56d306ba2df6d0e2119d
Dmitrii <dtiuterev@rambler.ru>
2022-08-29 11:15:18 +0300

    added Ex15

---
+++ Ex15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определение выходного дня по вводимой цифре дня недели 
+Console.WriteLine("Введи цифру, обозначающую соответствующий день недели (от 1 до 7): ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber > 0 && dayNumber < 6)
+    
+  Console.WriteLine("Это рабочий день");
+
+else if (dayNumber == 6 || dayNumber == 7)    
+
+    Console.WriteLine("Это выходной день");
+    
+    else Console.WriteLine("Введено некорректное число");
