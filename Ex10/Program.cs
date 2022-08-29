85074425686786db9853882932d17d325786daa6
Dmitrii <dtiuterev@rambler.ru>
2022-08-29 11:13:20 +0300

    added Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывод второй цифры трехзначного вводимого числа
+Console.WriteLine("Введите трехзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+string stringNumberText = Convert.ToString(stringNumber);
+if (stringNumberText.Length == 3)
+Console.WriteLine("Вторая цифра этого числа "+stringNumber[1]);
+else
+Console.WriteLine("Введено некорректное число");
